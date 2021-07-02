d264c4602221b20dfe1cd813f49b6c740c7604ba
Jean Everton <jeanever39@gmail.com>
2021-07-02 16:31:29 -0300

    Organizando as pastas

--- Projeto/client/js/index.js
+++ Projeto/client/js/index.js
@@ -1,7 +1,13 @@
+// lista parta varer com o DOM os campos da index, é uma api que deixa 
+// ver os campo através do css
 var campos = [
-    document.querySelector('#data'),
-    document.querySelector('#quantidade'),
-    document.querySelector('#valor'),
+  document.querySelector("#data"),
+  document.querySelector("#quantidade"),
+  document.querySelector("#valor"),
 ];
 
 console.log(campos);
+
+document.querySelector('.from').addEventListener('submint', function(event) {
+    alert('Oi');
+});
