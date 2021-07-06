7be5315c4cbf6af41751b0651ebb884d7ba2a129
Jean Everton <jeanever39@gmail.com>
2021-07-06 17:58:36 -0300

    deixando o dom mais inteligente

--- frame/client/js/app/controllers/NegociacaoController.js
+++ frame/client/js/app/controllers/NegociacaoController.js
@@ -7,21 +7,33 @@ class NegociacaoController {
     this._inputData = $("#data"); // percorrendo o dom para pegar as informações dos campos do formulário
     this._inputQuantidade = $("#quantidade"); // percorrendo o dom para pegar as informações dos campos do formulário
     this._inputValor = $("#valor"); // percorrendo o dom para pegar as informações dos campos do formulário
+    this._listaNegociacoes = new ListaNegociacoes();
   }
 
- adiciona(event) {
+  adiciona(event) {
+    event.preventDefault();
 
-        event.preventDefault();
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._limpaFormulario();
 
-        let negociacao = new Negociacao(
-          DateHelper.textoParaData(this._inputData.value),
-          this._inputQuantidade.value,
-          this._inputValor.value
-        );
+    console.log(this._listaNegociacoes.negociacoes);
+  }
 
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+  }
 
-       // console.log(negociacao);
+  _limpaFormulario() {
+    this._inputData.value = "";
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
 
-        console.log(DateHelper.dataParaTexto(negociacao.Data));
-    }
+    this._inputData.focus();
+
+    // focus assim que o usuario preencher os dados e inserir volta o cursor para a data
+  }
 }
