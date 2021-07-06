372d9a6c4776cbbf5aa92272a4178b9c29110277
Jean Everton <jeanever39@gmail.com>
2021-07-06 10:40:46 -0300

    Configurando a data

--- frame/client/js/app/controllers/NegociacaoController.js
+++ frame/client/js/app/controllers/NegociacaoController.js
@@ -6,17 +6,17 @@ class NegociacaoController {
     
         let $ = document.querySelector.bind(document); // transformando o document em uma variavel acessivel
     
-        this.inputData = $('#data'); // percorrendo o dom para pegar as informações dos campos do formulário
-        this.inputQuantidade = $('#quantidade'); // percorrendo o dom para pegar as informações dos campos do formulário
-        this.inputValor = $('#valor'); // percorrendo o dom para pegar as informações dos campos do formulário 
+        this._inputData = $('#data'); // percorrendo o dom para pegar as informações dos campos do formulário
+        this._inputQuantidade = $('#quantidade'); // percorrendo o dom para pegar as informações dos campos do formulário
+        this._inputValor = $('#valor'); // percorrendo o dom para pegar as informações dos campos do formulário 
 
   }
 
   adiciona(event) {
     event.preventDefault();
 
-    console.log(this.inputData.value);
-    console.log(this.inputQuantidade.value);
-    console.log(this.inputValor.value);
+   // let data = new Date(this._inputData.value.split('-')); // split serve como um separador
+    let data = new Date(this._inputData.value.replace(/-/g, ',')); // replece serve como um separador e troca de caractere 
+    console.log(data);
   }
 }
