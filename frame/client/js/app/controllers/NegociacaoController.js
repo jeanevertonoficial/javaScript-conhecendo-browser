5c513b09ffaa948b76dc26f532d876140208e5d7
Jean Everton <jeanever39@gmail.com>
2021-07-06 09:48:40 -0300

    percorrendoo Dom com uma performace melhor

--- frame/client/js/app/controllers/NegociacaoController.js
+++ frame/client/js/app/controllers/NegociacaoController.js
@@ -1,6 +1,22 @@
 class NegociacaoController {
+
+  constructor() {
+    
+        //alert('chamando o controller');
+    
+        let $ = document.querySelector.bind(document); // transformando o document em uma variavel acessivel
+    
+        this.inputData = $('#data'); // percorrendo o dom para pegar as informações dos campos do formulário
+        this.inputQuantidade = $('#quantidade'); // percorrendo o dom para pegar as informações dos campos do formulário
+        this.inputValor = $('#valor'); // percorrendo o dom para pegar as informações dos campos do formulário 
+
+  }
+
   adiciona(event) {
     event.preventDefault();
-    alert("Chamei ação no controller");
+
+    console.log(this.inputData.value);
+    console.log(this.inputQuantidade.value);
+    console.log(this.inputValor.value);
   }
 }
