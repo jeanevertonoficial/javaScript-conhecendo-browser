716f7ba2f9b6725aeb2ed7c147616c3d65a6ded1
Jean Everton <jeanever39@gmail.com>
2021-07-06 10:59:41 -0300

    Omitindo a função e passando =>, melhorando a sintaxe do map

--- frame/client/js/app/controllers/NegociacaoController.js
+++ frame/client/js/app/controllers/NegociacaoController.js
@@ -1,31 +1,28 @@
 class NegociacaoController {
-
   constructor() {
-    
-        //alert('chamando o controller');
-    
-        let $ = document.querySelector.bind(document); // transformando o document em uma variavel acessivel
-    
-        this._inputData = $('#data'); // percorrendo o dom para pegar as informações dos campos do formulário
-        this._inputQuantidade = $('#quantidade'); // percorrendo o dom para pegar as informações dos campos do formulário
-        this._inputValor = $('#valor'); // percorrendo o dom para pegar as informações dos campos do formulário 
+    //alert('chamando o controller');
 
+    let $ = document.querySelector.bind(document); // transformando o document em uma variavel acessivel
+
+    this._inputData = $("#data"); // percorrendo o dom para pegar as informações dos campos do formulário
+    this._inputQuantidade = $("#quantidade"); // percorrendo o dom para pegar as informações dos campos do formulário
+    this._inputValor = $("#valor"); // percorrendo o dom para pegar as informações dos campos do formulário
   }
 
   adiciona(event) {
     event.preventDefault();
 
-    let data = new Date(...
-      this._inputData.value
-        .split('-') // lendo o array da data e substituindo os separadores por -
-        .map(function(item, indice) { // map uma função para criar um array no data, item é ano mes e dia cada um vira um item, o indice que é passado no parametro é a posição 
-          if(indice == 1) { // posição do mes que fica no meio por isto 1, a contagem é feita de 0 em diante
-            return item - 1; // retorna o mes com 1 a menos para padronizar 
-          }
-          return item;
-        })
-        
-      );
-    console.log(data);
+    let data = new Date(
+      ...this._inputData.value
+        .split("-") // lendo o array da data e substituindo os separadores por -
+        .map((item, indice) => item - indice % 2)
+    );
+
+    let negociacao = new Negociacao(
+      data,
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+    console.log(negociacao);
   }
 }
